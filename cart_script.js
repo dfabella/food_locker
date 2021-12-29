a83ca22ec6a9ef8679dc9a2463b7a45e1d81be44
dfabella <dfabella@users.noreply.github.com>
2021-12-28 20:21:29 -0800

    painfully long increase/decrease quantity feature

--- cart_script.js
+++ cart_script.js
@@ -66,28 +66,218 @@ const totalPrice = document.getElementById('price');
 
 totalPrice.innerText = sum.toFixed(2);
 
+function findItemNum(itemName) {
+// Finds a given item's number suffix (i.e. 1-6) so it can be retrieved from the API
+    let itemNum = "";
+    for(let i = 1; i <= spaces.length; i++) {
+        let keyVal = localStorage.getItem("name" + i);
+        if (keyVal === itemName) {
+            itemNum = i.toString();
+        }
+    }
+    return itemNum;
+}
+
+/*--------increment and decrement quantity--------*/
+/*--------------------------------------------------*/
+ //ITEM 1
+ const plus = document.getElementById('plus1'),
+ minus = document.getElementById('minus1'),
+ count = document.getElementById('amountVal1'),
+ priceVal1 = document.getElementById('cartPriceVal1'),
+ name = document.getElementById('cartItem1').innerText;
+
+let a = Number(count.innerText);
+let price1 = Number(priceVal1.innerText) / a;
+let newPrice1 = price1;
+
+plus.addEventListener("click", ()=>{
+    a++;
+    newPrice1 = a * price1;
+
+    count.innerText = a;
+    priceVal1.innerText = newPrice1.toFixed(2);
+
+    localStorage.setItem("a", a);
+    localStorage.setItem("newPrice1", newPrice1);
+
+    console.log(a);
+    console.log(newPrice1);
+
+    location.reload();
+
+});
+
+minus.addEventListener("click", ()=>{
+    if(a > 1){
+        a--;
+        newPrice1 = a * price1;
+        priceVal1.innerText = "";
+        priceVal1.innerText = newPrice1.toFixed(2);
+    }
+    count.innerText = a;
+
+    localStorage.setItem("a", a);
+    localStorage.setItem("newPrice1", newPrice1);
+
+    console.log(a);
+    console.log(newPrice1);
+
+    location.reload();
+});
+
+//ITEM 2
+const plus2 = document.getElementById('plus2'),
+ minus2 = document.getElementById('minus2'),
+ count2 = document.getElementById('amountVal2'),
+ priceVal2 = document.getElementById('cartPriceVal2'),
+ name2 = document.getElementById('cartItem2').innerText;
+
+let b = Number(count2.innerText);
+let price2 = Number(priceVal2.innerText) / b;
+let newPrice2 = price2;
+
+plus2.addEventListener("click", ()=>{
+    b++;
+    newPrice2 = b * price2;
+
+    count2.innerText = b;
+    priceVal2.innerText = newPrice2.toFixed(2);
+
+    localStorage.setItem("b", b);
+    localStorage.setItem("newPrice2", newPrice2);
+
+    console.log(b);
+    console.log(newPrice2);
+
+    location.reload();
+
+});
+
+minus2.addEventListener("click", ()=>{
+    if(b > 1){
+        b--;
+        newPrice2 = b * price2;
+        priceVal2.innerText = "";
+        priceVal2.innerText = newPrice2.toFixed(2);
+    }
+    count2.innerText = b;
+
+    localStorage.setItem("b", b);
+    localStorage.setItem("newPrice2", newPrice2);
+
+    console.log(b);
+    console.log(newPrice2);
+
+    location.reload();
+});
+
+//ITEM 3
+const plus3 = document.getElementById('plus3'),
+ minus3 = document.getElementById('minus3'),
+ count3 = document.getElementById('amountVal3'),
+ priceVal3 = document.getElementById('cartPriceVal3'),
+ name3 = document.getElementById('cartItem3').innerText;
+
+let c = Number(count3.innerText);
+let price3 = Number(priceVal3.innerText) / c;
+let newPrice3 = price3;
+
+plus3.addEventListener("click", ()=>{
+    c++;
+    newPrice3 = c * price3;
+
+    count3.innerText = c;
+    priceVal3.innerText = newPrice3.toFixed(2);
+
+    localStorage.setItem("c", c);
+    localStorage.setItem("newPrice3", newPrice3);
+
+    console.log(c);
+    console.log(newPrice3);
+
+    location.reload();
+
+});
+
+minus3.addEventListener("click", ()=>{
+    if(c > 1){
+        c--;
+        newPrice3 = c * price3;
+        priceVal3.innerText = "";
+        priceVal3.innerText = newPrice3.toFixed(2);
+    }
+    count3.innerText = c;
+
+    localStorage.setItem("c", c);
+    localStorage.setItem("newPrice3", newPrice3);
+
+    console.log(c);
+    console.log(newPrice3);
+
+    location.reload();
+});
+
+//ITEM 4
+const plus4 = document.getElementById('plus4'),
+ minus4 = document.getElementById('minus4'),
+ count4 = document.getElementById('amountVal4'),
+ priceVal4 = document.getElementById('cartPriceVal4'),
+ name4 = document.getElementById('cartItem4').innerText;
+
+let d = Number(count4.innerText);
+let price4 = Number(priceVal4.innerText) / d;
+let newPrice4 = price4;
+
+plus4.addEventListener("click", ()=>{
+    d++;
+    newPrice4 = d * price4;
+
+    count4.innerText = d;
+    priceVal4.innerText = newPrice4.toFixed(2);
+
+    localStorage.setItem("d", d);
+    localStorage.setItem("newPrice4", newPrice4);
+
+    console.log(d);
+    console.log(newPrice4);
+
+    location.reload();
+
+});
+
+minus4.addEventListener("click", ()=>{
+    if(d > 1){
+        d--;
+        newPrice4 = d * price4;
+        priceVal4.innerText = "";
+        priceVal4.innerText = newPrice4.toFixed(2);
+    }
+    count4.innerText = d;
+
+    localStorage.setItem("d", d);
+    localStorage.setItem("newPrice4", newPrice4);
+
+    console.log(d);
+    console.log(newPrice4);
+
+    location.reload();
+});
+
+
+
 //DELETE ITEM
 function deleteItem(r) {
     var i = r.parentNode.parentNode.rowIndex;
     var name = document.getElementById("cartItem" + i).innerText; //get item's name
 
     // retrieve key names to delete based on item's name
-    let toDeleteID = "";
-    for(let i = 1; i <= spaces.length; i++) {
-        let keyVal = localStorage.getItem("name" + i);
-        if (keyVal === name) {
-            toDeleteID = i.toString();
-        }
-    }
+    let toDeleteID = findItemNum(name);
 
     // delete those key names from localStorage
-    const itemDetails = ["name", "newPrice"];
-    const amounts = ["","a", "b", "c", "d", "e", "f"]
-    for(const itemDetail of itemDetails) {
-        localStorage.removeItem(itemDetail + toDeleteID);
-        localStorage.removeItem(amounts[toDeleteID]);
-    }
-
+    localStorage.removeItem("name"+toDeleteID);
+    localStorage.removeItem("newPrice"+toDeleteID);
+    localStorage.removeItem(amounts[String.fromCharCode(96 + toDeleteID)]);
     location.reload();
 }
 
@@ -96,9 +286,7 @@ function toOrderProg() {
     if (totalPrice.innerText != "0.00") {
         window.location.href = 'orderProgress.html';
     }
-
 }
-
 const clearButton = document.getElementById('cartTitle');
 clearButton.addEventListener("click", ()=>{
     localStorage.clear();
--- orderProgressScript.js
+++ orderProgressScript.js
@@ -5,6 +5,158 @@ function completeOrder() {
     timeout = setTimeout(load, 1500);
 }
 
+let zeroPrice = Number(0);
+
+cartItem1 = document.getElementById('cartItem1');
+cartItem2 = document.getElementById('cartItem2');
+cartItem3 = document.getElementById('cartItem3');
+cartItem4 = document.getElementById('cartItem4');
+cartItem5 = document.getElementById('cartItem5');
+cartItem6 = document.getElementById('cartItem6');
+
+cartPriceVal1 = document.getElementById('cartPriceVal1');
+cartPriceVal2 = document.getElementById('cartPriceVal2');
+cartPriceVal3 = document.getElementById('cartPriceVal3');
+cartPriceVal4 = document.getElementById('cartPriceVal4');
+cartPriceVal5 = document.getElementById('cartPriceVal5');
+cartPriceVal6 = document.getElementById('cartPriceVal6');
+
+amountVal1 = document.getElementById('amountVal1');
+amountVal2 = document.getElementById('amountVal2');
+amountVal3 = document.getElementById('amountVal3');
+amountVal4 = document.getElementById('amountVal4');
+amountVal5 = document.getElementById('amountVal5');
+amountVal6 = document.getElementById('amountVal6');
+
+
+const spaces = [];
+spaces[0] = cartItem1;
+spaces[1] = cartItem2;
+spaces[2] = cartItem3;
+spaces[3] = cartItem4;
+spaces[4] = cartItem5;
+spaces[5] = cartItem6;
+
+const prices = [];
+prices[0] = cartPriceVal1;
+prices[1] = cartPriceVal2;
+prices[2] = cartPriceVal3;
+prices[3] = cartPriceVal4;
+prices[4] = cartPriceVal5;
+prices[5] = cartPriceVal6;
+for(let i = 0; i < prices.length; i++){
+    prices[i].innerText = zeroPrice.toFixed(2);
+}
+const amounts = [];
+amounts[0] = amountVal1;
+amounts[1] = amountVal2;
+amounts[2] = amountVal3;
+amounts[3] = amountVal4;
+amounts[4] = amountVal5;
+amounts[5] = amountVal6;
+
+//FIRST CART ITEM
+let name1 = localStorage.getItem('name1');
+console.log(name1);
+let price1 = Number(localStorage.getItem("newPrice1"));
+console.log("price 1 = ", price1);
+let amount1 = Number(localStorage.getItem("a"));
+
+    for(let i = 0; i < spaces.length; i++){
+        if(spaces[i].innerHTML == ""){
+            spaces[i].innerHTML = name1;
+            prices[i].innerText = price1.toFixed(2);
+            amounts[i].innerText = amount1;
+            break;
+        }
+    }
+
+//SECOND CART ITEM
+let name2 = localStorage.getItem('name2');
+console.log(name2);
+let price2 = Number(localStorage.getItem("newPrice2"));
+console.log("price 2 = ", price2);
+let amount2 = Number(localStorage.getItem("b"));
+
+    for(let i = 0; i < spaces.length; i++){
+        if(spaces[i].innerHTML == ""){
+            spaces[i].innerHTML = name2;
+            prices[i].innerText = price2.toFixed(2);
+            amounts[i].innerText = amount2;
+            break;
+        }
+    }
+
+//THIRD CART ITEM
+let name3 = localStorage.getItem('name3');
+console.log(name3);
+let price3 = Number(localStorage.getItem("newPrice3"));
+console.log("price 3 = ", price3);
+let amount3 = Number(localStorage.getItem("c"));
+
+    for(let i = 0; i < spaces.length; i++){
+        if(spaces[i].innerHTML == ""){
+            spaces[i].innerHTML = name3;
+            prices[i].innerText = price3.toFixed(2);
+            amounts[i].innerText = amount3;
+            break;
+        }
+    }
+
+//FOURTH CART ITEM
+let name4 = localStorage.getItem('name4');
+console.log(name4);
+let price4 = Number(localStorage.getItem("newPrice4"));
+console.log("price 4 = ", price4);
+let amount4 = Number(localStorage.getItem("d"));
+
+    for(let i = 0; i < spaces.length; i++){
+        if(spaces[i].innerHTML == ""){
+            spaces[i].innerHTML = name4;
+            prices[i].innerText = price4.toFixed(2);
+            amounts[i].innerText = amount4;
+            break;
+        }
+    }
+
+//FIFTH CART ITEM
+let name5 = localStorage.getItem('name5');
+console.log(name5);
+let price5 = Number(localStorage.getItem("newPrice5"));
+console.log("price 5 = ", price5);
+let amount5 = Number(localStorage.getItem("e"));
+
+    for(let i = 0; i < spaces.length; i++){
+        if(spaces[i].innerHTML == ""){
+            spaces[i].innerHTML = name5;
+            prices[i].innerText = price5.toFixed(2);
+            amounts[i].innerText = amount5;
+            break;
+        }
+    }
+
+//SIXTH CART ITEM
+let name6 = localStorage.getItem('name6');
+console.log(name6);
+let price6 = Number(localStorage.getItem("newPrice6"));
+console.log("price 6 = ", price6);
+let amount6 = Number(localStorage.getItem("f"));
+
+    for(let i = 0; i < spaces.length; i++){
+        if(spaces[i].innerHTML == ""){
+            spaces[i].innerHTML = name6;
+            prices[i].innerText = price6.toFixed(2);
+            amounts[i].innerText = amount6;
+            break;
+        }
+    }
+
+const totalPrice = document.getElementById('price');
+let sum = price1 + price2 + price3 + price4 + price5 + price6;
+
+totalPrice.innerText = sum.toFixed(2);
+
+
 function load() {
     if (i === 0) {
         i = 1;
