151662f4553ae0d6d42c5a4f9740cd4e127c922c
Ryan Wong <77073959+ry4nwong@users.noreply.github.com>
2021-12-30 00:56:12 -0800

    Create enterPin_script.js

---
+++ enterPin_script.js
@@ -0,0 +1,22 @@
+function buttonClick(click_value) {
+    let ele = document.getElementById('pinEntry');
+    ele.innerHTML += click_value;
+}
+
+function clearPin() {
+    let ele = document.getElementById('pinEntry');
+    ele.innerHTML = "PIN: ";
+}
+
+function unlockLocker() {
+    let ele = document.getElementById('pinEntry');
+    let splitEle = ele.innerHTML.split('PIN: ');
+    let finalPin = splitEle[1];
+
+    if (finalPin.length != 4) {
+       alert("PIN must be 4 digits long!");
+       clearPin();
+    } else {
+       dbHasPin(finalPin);
+    }
+ }
