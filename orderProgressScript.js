79bd702bedf7a88050a9f3b1f01d23aaaf8cfc44
dfabella <dfabella@users.noreply.github.com>
2022-01-03 17:04:57 -0800

    undid changes to order-preparing png

--- orderProgressScript.js
+++ orderProgressScript.js
@@ -15,13 +15,13 @@ function load() {
         var id = setInterval(frame, 10);
     }
         function frame() {
-            if (width == 60) {
+            if (elem.style.width == "96%") {
                 document.getElementById('ready').src="./assets/order-ready1.png";
                 progressText.textContent = "Order Ready For Pick-Up!"
                 clearInterval(id);
 
-            } else if (width == 46){
-                document.getElementById('poopoo').src="./assets/order-preparing1.png";
+            } else if (elem.style.width == "46%"){
+                document.getElementById('preparing').src="./assets/order-preparing1.png";
                 progressText.textContent = "Order Preparing...";
                 width += 0.2;
                 elem.style.width = width + "%";
