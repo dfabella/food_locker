0b475965056e39848ea140361ed585ca83e329aa
dfabella <dfabella@users.noreply.github.com>
2022-01-03 16:46:31 -0800

    oops: splashScreen is animated now

--- js/splashScreenScript.js
+++ js/splashScreenScript.js
@@ -1,12 +1,12 @@
-// var delayInMilliseconds = 1500;
+var delayInMilliseconds = 1500;
 let splashscreen = document.getElementById('welcome');
 
-// //splashscreen fades out on init
-// function fadeSplash() {
-//     setTimeout(fadeOut, delayInMilliseconds);
-// }
-//
-// function fadeOut() {
-//     splashscreen.classList.toggle('fade');
-//     window.location.href="menu.html";
-// }
+//splashscreen fades out on init
+function fadeSplash() {
+    setTimeout(fadeOut, delayInMilliseconds);
+}
+
+function fadeOut() {
+    splashscreen.classList.toggle('fade');
+    window.location.href="menu.html";
+}
